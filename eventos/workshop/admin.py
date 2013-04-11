e97a225c9c2a357210e4a5fd52dfcfa48277a7c7
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2013-04-11 16:13:34 -0700

    Added filters to admin

--- eventos/workshop/admin.py
+++ eventos/workshop/admin.py
@@ -4,7 +4,25 @@ from django.contrib import admin
 from eventos.workshop.models import Evento, Palestrante, Palestra, \
     Participante
 
-admin.site.register(Evento)
-admin.site.register(Palestrante)
-admin.site.register(Palestra)
-admin.site.register(Participante)
+
+class EventoAdmin(admin.ModelAdmin):
+    search_fields = ['titulo', 'descricao']
+    list_filter = ['data']
+
+
+class PalestranteAdmin(admin.ModelAdmin):
+    search_fields = ['nome_palestrante']
+
+
+class PalestraAdmin(admin.ModelAdmin):
+    search_fields = ['nome_palestra']
+
+
+class Participante(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+admin.site.register(Evento, EventoAdmin)
+admin.site.register(Palestrante, PalestranteAdmin)
+admin.site.register(Palestra, PalestraAdmin)
+admin.site.register(Participante, ParticipanteAdmin)
